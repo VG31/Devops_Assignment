ecdce56781c4634736aacf18a37cc4a539cf5334
Vivek G <111217691+VG31@users.noreply.github.com>
2025-06-21 17:58:01 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ sudo yum update -y
 sudo yum install docker -y
 sudo service docker start
 sudo usermod -aG docker ec2-user
-cd /home/ec2-user/password-generator
+cd /Devops_Assignment/PasswordApp
 docker build -t password-generator .
 docker run -d -p 80:80 password-generator
